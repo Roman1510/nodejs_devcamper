b8e336d8c3a48344cd74db8ea425a6d18fc9ff8b
Vinnick <romanvinnick@gmail.com>
2023-05-07 21:37:52 +0200

    added mongo connection

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const bootcamps = require('./routes/bootcamps') // routing file
-// const logger = require('./middleware/logger')
 const morgan = require('morgan')
-
+const connectDB = require('./config/db')
 dotenv.config({ path: './config/config.env' }) //env variabls
+
+connectDB()
 
 const app = express()
 
@@ -18,9 +19,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `server running on ${process.env.NODE_ENV} on port ${PORT}`
   )
 )
+
+process.on('unhandledRejection', (error) => {
+  console.error(`Unhandled rejection: ${error}`)
+  server.close(() => process.exit(1))
+})
