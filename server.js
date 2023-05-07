0f1d0ede79f1762bb986aa621d4e924729cc5b18
Vinnick <romanvinnick@gmail.com>
2023-05-07 19:41:28 +0200

    last changes before the mongodb setup

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require('./routes/bootcamps') // routing file
+// const logger = require('./middleware/logger')
+const morgan = require('morgan')
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' }) //env variabls
 
 const app = express()
+
+//check if we're no dev
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+//mounting routers
 app.use('/api/v1/bootcamps', bootcamps)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(
