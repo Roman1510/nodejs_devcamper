5a74bd7a41566eedbefc40a337c2801eb3270bc1
Vinnick <romanvinnick@gmail.com>
2023-05-17 22:21:52 +0200

    adding custom error handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const bootcamps = require('./routes/bootcamps') // routing file
 const morgan = require('morgan')
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 dotenv.config({ path: './config/config.env' }) //env variabls
 
@@ -18,14 +19,13 @@ if (process.env.NODE_ENV === 'development') {
 
 //mounting routers
 app.use('/api/v1/bootcamps', bootcamps)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
 const server = app.listen(
   PORT,
-  console.log(
-    `server running on ${process.env.NODE_ENV} on port ${PORT}`
-  )
+  console.log(`server running on ${process.env.NODE_ENV} on port ${PORT}`)
 )
 
 process.on('unhandledRejection', (error) => {
